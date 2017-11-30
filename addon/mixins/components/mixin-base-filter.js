78a500024952ca0ecb0cd5c9ad5789568df1c133
Vladislav Andrš <vladislav.andrs@gmail.com>
2017-11-30 15:53:39 +0100

    Comments added

--- addon/mixins/components/mixin-base-filter.js
+++ addon/mixins/components/mixin-base-filter.js
@@ -3,11 +3,15 @@ import { convertAccentedCharacters } from 'ember-c-base-pack/helpers/convert-acc
 
 export default Ember.Mixin.create({
 
-  rows           : [],
-  query          : '',
-  sortBy         : '',
-  sortReverse    : false,
-  filterProperty : 'fulltext',
+  // Inputs:
+  rows           : [],         // Must contain the input collection. 
+  query          : '',         // Must contain the filter query.
+  filterProperty : 'fulltext', // Must contain the name of the property which we use for filtering.
+  sortBy         : '',         // Can contain the name of the property we will use for sorting.
+  sortReverse    : false,      // Sort reverse?
+
+  // Output:
+  // rowsFiltered
 
   init() {
     this._super(...arguments);
@@ -15,11 +19,11 @@ export default Ember.Mixin.create({
     // Define computed property dynamically.
     // It is not possible to define computed property with variable in it.
     // If sort by is defined, we can sort.
-    Ember.defineProperty(this, 'rowsSorted', 
-      Ember.computed("rows.@each." + this.get("sortBy"), 'rows.length', 'sortBy', function(){
-        var sortBy = this.get("sortBy");
-        var rows = sortBy ? this.get("rows").sortBy(sortBy) : this.get("rows");
-        if (this.get("sortReverse")) {
+    Ember.defineProperty(this, 'rowsSorted',
+      Ember.computed('rows.@each.' + this.get('sortBy'), 'rows.length', 'sortBy', function(){
+        var sortBy = this.get('sortBy');
+        var rows = sortBy ? this.get('rows').sortBy(sortBy) : this.get('rows');
+        if (this.get('sortReverse')) {
           rows.reverse();
         }
         return rows;
@@ -30,22 +34,18 @@ export default Ember.Mixin.create({
 
   // Filtr rows.
   rowsFiltered: Ember.computed('query', 'rowsSorted.length', function() {
-    var searchQuery = this.get("query");
-    if (searchQuery === "" || searchQuery === undefined) {
-      return this.get("rowsSorted");
+    var searchQuery = this.get('query');
+    if (searchQuery === '' || searchQuery === undefined) {
+      return this.get('rowsSorted');
     }
     searchQuery        = convertAccentedCharacters([ searchQuery ]);
-    searchQuery        = searchQuery.replace(" ", ".*");
+    searchQuery        = searchQuery.replace(' ', '.*');
     var regExPattern   = '\\b.*' + searchQuery + '.*\\b';
     var regexp         = new RegExp(regExPattern,'gi');
-    var filterProperty = this.get("filterProperty");
+    var filterProperty = this.get('filterProperty');
     return this.get('rowsSorted').filter(function(row){
       return row.get(filterProperty).match(regexp);
     });
   }),
 
 });
-
-
-
-
