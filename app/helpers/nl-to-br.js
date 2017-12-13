4fc10ca4dc0c781a107e6cab621828cb0ac9b9bc
Vladislav Andrš <vladislav.andrs@gmail.com>
2017-12-13 14:04:51 +0100

    Function changed

--- app/helpers/nl-to-br.js
+++ app/helpers/nl-to-br.js
@@ -4,7 +4,7 @@ export function nlToBr(params) {
   var text = params[0];
   text = Ember.Handlebars.Utils.escapeExpression(text);
   text = text.replace(/(\r\n|\n|\r)/gm, '<br />');
-  return new Ember.Handlebars.SafeString(text);
+  return new Ember.String.htmlSafe(text);
 }
 
 export default Ember.Helper.helper(nlToBr);
