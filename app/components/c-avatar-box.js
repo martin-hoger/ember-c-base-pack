7bb10c2cf2902ffef33b20a7c0dff4e269cbcf90
Martin Höger <martin.hoger@gmail.com>
2017-12-15 21:44:46 +0100

    Avatar box

---
+++ app/components/c-avatar-box.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  classNames        : ['avatar-box'],
+  classNameBindings : ['rounded'],
+  fontSize          : 35,
+  fontWeight        : 35,
+  size              : 75,
+  rounded           : true,
+
+});
+
+
