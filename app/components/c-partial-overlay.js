39c2dfab1fe1e044afa1b574567d726a4170a3b9
Martin Höger <martin.hoger@gmail.com>
2017-12-15 14:59:23 +0100

    Directories moved

--- app/components/c-partial-overlay.js
+++ app/components/c-partial-overlay.js
@@ -1,14 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames : ["content"], 
+
   visible    : false,
 
   actions: {
     close() {
       // If there is onClose action, fire the function.
-      if (this.attrs && this.attrs.onClose !== undefined) {
-        this.attrs.onClose()
+      if (this.get("onClose")) {
+        this.get("onClose")();
       }
     }
   }
