456f449a9521d7fe21a991044a9a197c57c7a1ba
Martin Höger <martin.hoger@gmail.com>
2017-12-21 13:54:16 +0100

    Type removed

--- app/components/c-image-thumbnail.js
+++ app/components/c-image-thumbnail.js
@@ -5,7 +5,7 @@ export default Ember.Component.extend({
   attributeBindings : ['style'],
   height            : 100,
   width             : 100,
-  backgroundSize    : 'cover';
+  backgroundSize    : 'cover',
 
   style: Ember.computed('height', 'width', 'resolvedUrl', 'rounded', function() {
     // console.log('height: ', this.get('height'));
