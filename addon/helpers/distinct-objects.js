9627a04782ee5de802d22f492f9ad43ce201f96e
Vladislav Andrš <vladislav.andrs@gmail.com>
2018-01-10 22:19:54 +0100

    distinct-object helper created and tested

---
+++ addon/helpers/distinct-objects.js
@@ -0,0 +1,50 @@
+/*
+
+distinctObjects(items, propertyName)
+inputs: items(array of objects) you want to go through and propertyName(string)
+you want to test.
+output: array of propertyName objects, also counts the number of occurrences
+of this property in distinctObjectsCount.
+
+Example of use for making array of users in showen articles (rowsFiltered)
+and counting number of their articles:
+
+users : Ember.computed('rowsFiltered', function() {
+  var rowsFiltered = this.get('rowsFiltered');
+  var users = distinctObjects([rowsFiltered, 'user']);
+  return users.sortBy('distinctObjectsCount').reverse();
+
+*/
+
+import Ember from 'ember';
+
+export function distinctObjects(params) {
+  var items = params[0];
+  var propertyName = params[1];
+  var itemsArray = [];
+  items.forEach(function(item) {
+    // create array items with this propertyName:
+    var objects = item.get(propertyName);
+    // if it's not an array, make it. Because then we can use another forEach
+    // and doesn't matter, if there is only one object, or more objects
+    // 1 user of the article VS. many categories of the article
+    if (!Ember.isArray(objects)) {
+      objects = [objects]
+    }
+    objects.forEach(function(object){
+      // find item with this id:
+      var propertyArrayItem = itemsArray.findBy('id', object.get('id'));
+      if (!propertyArrayItem)  {  // item with this id not yet included, so add
+        propertyArrayItem = object;
+        propertyArrayItem.set('distinctObjectsCount', 0);  // number will be counted
+        itemsArray.push(propertyArrayItem);
+      }
+      // increment number of objects:
+      propertyArrayItem.incrementProperty('distinctObjectsCount');
+    });
+  });
+
+  return itemsArray;
+}
+
+export default Ember.Helper.helper(distinctObjects);
