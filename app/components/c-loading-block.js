699bb3c584c5ed0be6a0e1bf17f282f7954d02da
Martin Höger <martin.hoger@gmail.com>
2018-08-07 11:04:57 +0200

    Loading block

--- app/components/c-loading-block.js
+++ app/components/c-loading-block.js
@@ -1,27 +1,31 @@
 import Component from '@ember/component';
-import RSVP from 'rsvp';
+// import RSVP from 'rsvp';
 
 export default Component.extend({
 
-  isLoaded : false,
+  // isLoaded : false,
+  // init() {
+  //   this._super(...arguments);
+  //
+  //   RSVP.hash(this.get('model')).then((resolvedModel) => {
+  //     this.set('model', resolvedModel);
+  //     this.set('isLoaded', true);
+  //   });
+  // },
 
-  /**
-   * This component waits (shows loaded) until the data are resolved.
-   * Better way is to use RSVP hash at the router at the model().
-   *
-   * {{#c-loading-block model=model}}
-   *    {{model.client.name}}
-   * {{/c-loading-block}}
-   */
-  init() {
-    this._super(...arguments);
 
-    RSVP.hash(this.get('model')).then((resolvedModel) => {
-      this.set('model', resolvedModel);
-      this.set('isLoaded', true);
+  isLoaded: computed('model.@each.isPending', function () {
+    console.log('c-loading-block.js is deprecated, please user RSVP.hash() in the route.');
+    var models   = this.get('models');
+    var isLoaded = true;
+    models.forEach(function (model) {
+      if (model.get('isPending')) {
+        isLoaded = false;
+      }
     });
-  },
 
+    return isLoaded;
+  }),
 
 });
 
