42a1b551263acb2107049252241013b044cd93bb
Vladislav Andrš <vladislav.andrs@gmail.com>
2019-02-04 18:39:39 +0100

    c-modal-window improvement

--- app/components/c-modal-window.js
+++ app/components/c-modal-window.js
@@ -1,58 +1,55 @@
 /*
-Possible to create multiple overlay windows. If second overlay is opened from first
-overlay, new one has 'maxLeft' position and fist is moved left by computed
-property. If third is opened from second, third has again 'maxLeft' position and
-1. - 2. are moved to 100 and 200 px left. Nice :-)
-  - if (vissible == false) { class="hidden" is added to the element }
-  - maxLength is the possition of the last opened overlay, default 300px.
+  Modal window with close button, dark transparent background.
+  Can be closed also by clicking transparent background.
+  Default visible, width, max-height - possible to set another.
 
-First open of the overlay content is animated from right to left (even if mouse is not on it).
-When mouse left the overlay content, it is moved right after 1s waiting.
-=> The user see more of the backgroud window and can click it.
-When mouse is back on content, it is moved immediately left to the initial position.
+  Usage:
+  {{#c-modal-window}}
+    Content of the modal window
+  {{/c-modal-window}}
 
-Usage:
-{{#c-partial-overlay visible=true onClose=(route-action "closeWindow")}}
-  This is overlay 2
-  {{#link-to 'application.c-form-test.over1.over2.over3'}}
-    This is a link to overlay 3
-  {{/link-to}}
-  {{outlet}}
-{{/c-partial-overlay}}
+  {{#c-modal-window visible=true width="50%" maxHeigh="50%"}}
+    Content of the modal window
+  {{/c-modal-window}}
+
+  {{#c-modal-window visible=true width=300}}
+    Content of the modal window
+  {{/c-modal-window}}
 
 */
 
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 import { htmlSafe } from '@ember/string';
-import { scheduleOnce } from '@ember/runloop';
 import { later } from '@ember/runloop';
 
 export default Component.extend({
   classNames : ['modal-window-wrap'],
-  // classNameBindings: ['visible::hidden', 'focused:focused:blurred', 'initClass:init-class:'],
-  visible          : true,
-  maxLeft          : 300,
-  firstOpen        : true,  // first overlay open => overlay stays left, even if mouse is not on it
-  focused          : true,  // first overlay open
+  classNameBindings: ['initClass:init-class:'],
   initClass        : true,
+  visible          : true,
+  // default sizes:
+  maxHeight        : '80%',
+  width            : '600',
 
+  // after inserting element wait and remove init-class => animate move from left to right
+  // VA to MH : zatím to nefunguje, respektive funguje remove init-class, ale žádnou animaci neudělá...
+  didInsertElement() {
+    later(() => {
+      this.set('initClass', false);
+    }, 1);
+  },
 
-  // Generate style according number of overlays.
-  style: computed('session.cPartialoverlayIds.[]', function() {
-    var styles    = [];
-    var overlayId = this.get('overlayId');
-    var overlayIds = this.get('session.cPartialoverlayIds');
+  // style = Width: px or %, max-width
+  style: computed('width', 'maxHeigh', function() {
+    var maxHeigh = this.get('maxHeigh');
+    var width = this.get('width').toString();
+    if (width.substr(-1) != '%') {      // width: px or %
+      width = width + 'px'              // px
+    }
+    var style = (`width: ${width}; max-height: ${maxHeigh}`);
 
-    // Set width for multiple windows.
-    var width = 80;
-    // if (overlayIds) {
-    //   var index = overlayIds.indexOf(overlayId);
-    //   width = 100 - 20 / overlayIds.length * (index + 1);
-    // }
-    styles.push(`width: ${width}%`);
-
-    return htmlSafe(styles.join(';'));
+    return htmlSafe(style);
   }),
 
   actions: {
@@ -62,32 +59,6 @@ export default Component.extend({
         this.get('onClose')();
       }
     },
-
-    // Overlay content moving right (CSS), when the mouse left the overlay (after 1s waiting).
-    // First overlay open => overlay stays left, even if mouse is not on it
-    contentFocused(status) {
-      if (this.get('firstOpen')) {
-        this.set('firstOpen', false);
-        return;
-      }
-      // is the mouse on overlay content? Global variable.
-      this.set('mouseOnContent', status);
-
-      // content focused: move window left immediately
-      if (status) {
-        this.set('focused', status);
-      }
-
-      // mouse left the content: wait and if mouse is not on content back, then move window right
-      if (!status) {
-        later(( () => {
-          if (!this.get('mouseOnContent')) {
-            this.set('focused', false);
-          }
-        }), 300);
-      }
-
-    }
 
   }
 
