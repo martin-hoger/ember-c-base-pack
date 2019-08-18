5ffc22114a9c933c19ffad239bdb71a0bec15c78
Lukáš Höger <lukas.hoger@gmail.com>
2019-08-18 13:06:38 +0200

    Added tabs.

---
+++ app/components/c-tab-pane.js
@@ -0,0 +1,38 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  didRegister : false,
+
+  //Register pane.
+  registerWithParent() {
+    if (!(this.didRegister)) {
+      let parent = this.get('parent');
+      if (parent) {
+	parent.registerPane(this);
+	this.didRegister = true;
+      }
+    }
+  },
+
+  //Register pane with parents - all life cykle hooks.
+  didReceiveAttrs() {
+    this._super(...arguments);
+    this.registerWithParent();
+  },
+
+  willRender() {
+    this._super(...arguments);
+    this.registerWithParent();
+  },
+
+  //UnRegister pane with parent - destroy hook.
+  willDestroyElement() {
+    this._super(...arguments);
+    let parent = this.get('parent');
+    if(this.didRegister && parent) {
+      parent.removePane(this);
+      this.didRegister = false;
+    }
+  }
+});
+
