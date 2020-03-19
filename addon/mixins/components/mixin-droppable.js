dba707229fb3a0e50b18c08875124a96834c4eeb
Vladislav Andrš <vladislav.andrs@gmail.com>
2020-03-19 18:13:39 +0100

    Sort array after dropping dragged row

--- addon/mixins/components/mixin-droppable.js
+++ addon/mixins/components/mixin-droppable.js
@@ -116,6 +116,53 @@ export default Mixin.create({
     event.preventDefault();
     this._dragOver(...arguments);
     return false;
-  })
+  }),
+
+  /**
+    Sort 'rows', when gragged row is dropped to the target row. Rows are re-orderd
+    and weight is updated to 0..numberOfElements-1
+
+    Usage, if we use default 'weight' as a weight key and 'id' ad ID key:
+    var sorted = this._getGragDropSortedArray(targetProperty, draggedProperty, properties);
+    Usage, if we use 'weight' as a weight key:
+    var sorted = this._getGragDropSortedArray(targetProperty, draggedProperty, properties, 'property.id');
+  */
+  _getGragDropSortedArray(targetRow, draggedRow, rows, idKey, weightKey) {
+    // If variables not defined, use default:
+    idKey = (idKey) ? idKey : 'id';
+    weightKey = (weightKey) ? weightKey : 'weight';
+
+    var addWeight = false;
+    // Go through rows and find target row. Change weight of target row, dragged
+    // row and all next rows:
+    rows.forEach(function(row){
+      // Add weight? But not to the dragged row!
+      if (addWeight && row.get(idKey) != draggedRow.get(idKey)) {
+        row.set(weightKey, row.get(weightKey) + 1);
+      }
+      // Found target row:
+      if (row.get(idKey) == targetRow.get(idKey)) {
+        if (row.get(weightKey) >= draggedRow.get(weightKey)) {
+          // Dragging from top to the bottom: dragged.weight++
+          draggedRow.set(weightKey, row.get(weightKey) + 1);
+        } else {
+          // dragging from bottom to the top: dragged.weight = row.weight
+          // and add weight to this row and all next rows
+          draggedRow.set(weightKey, row.get(weightKey));
+          row.set(weightKey, row.get(weightKey) + 1);
+        }
+        // And all next rows: add weight to it
+        addWeight = true;
+      }
+    });
+    // Sort array by weight and then reindex, so weight will be again in interval
+    // 0 .. number of items-1
+    rows = rows.sortBy(weightKey);
+    rows.forEach(function(row, index){
+      row.set(weightKey, index);
+    });
+
+    return rows;
+  },
 
 });
