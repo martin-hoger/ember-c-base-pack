7ab8fd4f9e03af1f70874add9ccdf186700f6d98
Lukáš Höger <lukas.hoger@gmail.com>
2021-05-19 22:34:25 +0200

    Modified loading bar.

--- app/components/c-loading-bar.js
+++ app/components/c-loading-bar.js
@@ -1,7 +1,9 @@
 import Component from '@ember/component';
+import { inject } from '@ember/service';
 
 export default Component.extend({
-  elementId: 'loading-bar',
+  elementId  : 'loading-bar',
+  loadingBar : inject(),
 });
 
 
--- app/services/loading-bar.js
+++ app/services/loading-bar.js
@@ -2,13 +2,11 @@ import Service from '@ember/service';
 import $ from 'jquery';
 
 export default Service.extend({
-
   show: function () {
     $('body').addClass('loading');
   },
 
   hide: function () {
     $('body').removeClass('loading');
-  }
-
+  },
 });
