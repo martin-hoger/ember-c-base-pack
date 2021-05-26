d23d5a3f9bc8a65aefba00055c6f4d2e9502ef54
Lukáš Höger <lukas.hoger@gmail.com>
2021-05-26 21:53:05 +0200

    Extended notification center.

--- app/services/notification-center.js
+++ app/services/notification-center.js
@@ -1,22 +1,57 @@
+import { task, timeout } from 'ember-concurrency';
 import Service from '@ember/service';
-import $ from 'jquery';
 
 export default Service.extend({
-  params          : {},
-  itemTypeDefault : 'message', //Default item type.
+  params           : {},
+  itemTypeDefault  : 'message', //Default item type.
+  delayTimeDefault : 3000,
   
-  //- pryc jquery
-  //
+  //Show notification.
   show: function (params) {
-    $('#notification-center').addClass('show');
-    if (!params.itemType) {
-      params.itemType = this.get('itemTypeDefault');
-    }
+    document.querySelector('#notification-center').classList.add("show");
     this.set('params', params);
+
+    //Set default item type - for factory methods.
+    if (!this.get('params.itemType')) {
+      this.set('params.itemType', this.get('itemTypeDefault'));
+    }
+    
+    //Set display progress of notification.
+    this.set('params.displayProgess', true);
+
+    //Set default delay time.
+    if (this.get('params.autoHide') || (this.get('params.titleFinal'))) {
+      if (!this.get('params.delayTime')) {
+        this.set('params.delayTime', this.get('delayTimeDefault'));
+      }
+    }
+
+    //Automatic hide after defined delay time.
+    if (this.get('params.autoHide')) {
+      this.get('processHide').perform();
+    }
   },
 
+  //Hide notification.
   hide: function () {
-    $('#notification-center').removeClass('show');
-    this.set('params', {});
+    this.get('processHide').perform();
   },
+
+  //Process hide.
+  processHide: task(function * () {
+
+    //With delay.
+    if (this.get('params.delayTime')) {
+      if (!this.get('params.autoHide')) {
+        //Start final title content.
+        this.set('params.displayProgess', false);
+      }
+      yield timeout(this.get('params.delayTime'));
+    }
+
+    document.querySelector('#notification-center').classList.remove("show");
+    this.set('params', {});
+
+  }).restartable(),
+
 });
