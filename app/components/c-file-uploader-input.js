99e646e7013a195c0c05ce4d7777c47ead7dc2b0
Martin Höger <martin.hoger@gmail.com>
2024-09-12 12:58:18 +0200

    Uploading of multiple files

--- app/components/c-file-uploader-input.js
+++ app/components/c-file-uploader-input.js
@@ -2,28 +2,47 @@ import Ember from 'ember';
 import EmberUploader from 'ember-uploader';
 
 export default EmberUploader.FileField.extend({
-  multiple : true,
-  url      : '',
+  multiple: true,
+  url: '',
+  classNames: ['file-uploader'],
 
-  //File is added.
-  filesDidChange (files) {
-    const uploader  = EmberUploader.Uploader.create({
+  init() {
+    this._super(...arguments);
+    this.set('isDragging', false);
+  },
+
+  dragOver(event) {
+    event.preventDefault();
+    this.set('isDragging', true);
+  },
+
+  dragLeave(event) {
+    event.preventDefault();
+    this.set('isDragging', false);
+  },
+
+  drop(event) {
+    event.preventDefault();
+    this.set('isDragging', false);
+    const files = event.dataTransfer.files;
+    this.filesDidChange(files);
+  },
+
+  classNameBindings: ['isDragging:drag-over'],
+
+  filesDidChange(files) {
+    const uploader = EmberUploader.Uploader.create({
       url: this.get("url")
     });
 
     if (!Ember.isEmpty(files)) {
-      //File upload.
       uploader.upload(files, {}).then(data => {
         this.sendAction('successAction', data);
-        // this.sendAction('errorAction', data);
       });
-      
-      //When file is uploaded.
+
       uploader.on('progress', e => {
         this.sendAction('progressAction', e.percent);
       });
     }
   }
-
-
 });
